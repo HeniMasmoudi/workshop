ea06342b31a835ceb4cae7e7ecbd83c13a27de2e
Heni Masmoudi <heni.masmoudi@infor.com>
2022-02-25 23:04:02 +0100

    create month/year/ day of month features

--- src/features_processing/createfeatures.py
+++ src/features_processing/createfeatures.py
@@ -1,17 +1,29 @@
 import os
 import pandas as pd
 import numpy as np
-from datetime import datetime
 from staging import cleaning
 
 workshop_home = os.environ.get("workshop")
 
 
-def create_doy(home):
+def get_cleaned_data(home):
     data = cleaning.main(home)
+    return data
+
+
+def create_doy(data):
     data["doy"] = data.invoicedate.dt.dayofyear.astype(np.int32)
     return data
 
 
+def create_month_year(data):
+    data["year"] = pd.DatetimeIndex(data.invoicedate).year
+    data["month"] = pd.DatetimeIndex(data.invoicedate).month
+    data["day_of_month"] = pd.DatetimeIndex(data.invoicedate).day
+    return data
+
+
 if __name__ == "__main__":
-    data = create_doy(workshop_home)
+    data = get_cleaned_data(workshop_home)
+    data = create_doy(data)
+    data = create_month_year(data)
--- tests/test_features_processing.py
+++ tests/test_features_processing.py
@@ -15,10 +15,26 @@ def fixture_xmi_home():
     return folder_name
 
 
-def test_doy_creation(workshop_dir):
-    data = createfeatures.create_doy(workshop_dir)
-    assert "doy" in list(data.columns), "problem in creating day of year"
+@pytest.fixture(name="data", scope="module")
+def fixture_select_columns(workshop_dir):
+    data = createfeatures.get_cleaned_data(workshop_dir)
+    return data
+
+
+def test_doy_creation(data):
+    data_with_doy = createfeatures.create_doy(data)
+    assert "doy" in list(data_with_doy.columns), "problem in creating day of year"
     assert (
-        data["doy"].dtype == np.int32
+        data_with_doy["doy"].dtype == np.int32
     ), "problem in day of year type, expected np.int32"
     assert data.doy.shape[0] == 541909, "missed rows in doy, expected 541909"
+
+
+def test_mdy_creation(data):
+    data_with_doy = createfeatures.create_doy(data)
+    data_with_mdy = createfeatures.create_month_year(data_with_doy)
+    assert "year" in data_with_mdy.columns, "year is not found in processed data"
+    assert "month" in data_with_mdy.columns, "month is not found in processed data"
+    assert (
+        "day_of_month" in data_with_mdy.columns
+    ), "day of month is not found in processed data"
