4e4ec4738d73237138fcc3e58ce7dd829f572fe1
Heni Masmoudi <heni.masmoudi@infor.com>
2022-02-25 16:34:18 +0100

    staging process

--- src/staging/cleaning.py
+++ src/staging/cleaning.py
@@ -1,14 +1,35 @@
-import os 
-import zipfile as ZipFile
-import pandas as pd 
-from zipfile import ZipFile
-  
+import os
+import zipfile
+import pandas as pd
+
 workshop_home = os.environ.get("workshop")
 
 
-# specifying the zip file name
-file_name = os.path.join(workshop_home,"retail_data.zip")
-  
-# opening the zip file in READ mode
-# with ZipFile(file_name, 'r') as zip:
- 
+def fetch_data(home):
+    """
+    Description
+    ----------
+    read data from zip file
+
+    Parameters
+    ----------
+    home: data home directory
+
+    Returns
+    -------
+    data : pandas dataframe
+        retail data (8 columns, 541909 rows, mixed types)
+
+    """
+
+    # specifying the zip file name
+    folder = os.path.join(home, "retail_data.zip")
+    # opening the zip file in READ mode
+    with zipfile.ZipFile(folder) as zip:
+        with zip.open("Online_Retail.csv") as myZip:
+            data = pd.read_csv(myZip, encoding="unicode_escape")
+    return data
+
+
+if __name__ == "__main__":
+    retail_data = fetch_data(workshop_home)
