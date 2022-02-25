5ba9da0054e0693fe6117fab18a47be5911c67ae
Heni Masmoudi <heni.masmoudi@infor.com>
2022-02-25 18:59:37 +0100

    add data loader unit test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,3 +1,19 @@
 """
 testing ML script 
 """
+
+from os.path import dirname, abspath
+import pytest
+from staging import loader
+
+
+@pytest.fixture(name="workshop", scope="session")
+def fixture_xmi_home():
+    folder_name = dirname(dirname(abspath(__file__)))
+    return folder_name
+
+
+def test_loader(workshop):
+    data = loader.fetch_data(workshop)
+    columns = data.columns
+    assert len(columns) == 8, "error in loading data"
