c787cb45fcd4a66a7b4821bfe2952f14fad4deb7
khaoula kadri <khaoula.kadri@infor.com>
2022-03-29 17:57:39 +0100

    exercise 3

---
+++ exercises/exercise3.py
@@ -0,0 +1,15 @@
+def is_prime(number):
+    """Return True if *number* is prime."""
+    for element in range(2, number):  # Don't use 0 and 1
+        if number % element == 0:
+            return False
+    return True
+
+
+def find_next_prime(number):
+    """Find the closest prime number larger than *number*."""
+    index = number
+    while True:
+        index += 1
+        if is_prime(index):
+            return index
